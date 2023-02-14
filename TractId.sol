e9d5d68cd8e8f0abfd92df6182eaa4492397b9ae
SMARTRIB3 <94409591+smartrib3@users.noreply.github.com>
2023-02-13 22:09:44 -0800

    Update TractId.sol
    This Tract ID smart contract has been updated to include additional variables to store information about the Township and Range directions, the county where the land is located.

--- TractId.sol
+++ TractId.sol
@@ -12,20 +12,28 @@ contract TractID {
     string public plant; // Principal Meridian and Base Line - used to establish a geographic location
     uint public section; // Section - identifies the specific location of the land within a particular area
     uint public township; // Township - identifies the specific location of the land within a particular area
+    string public townshipDirection; // Direction of Township from baseline
     uint public range; // Range - identifies the specific location of the land within a particular area
+    string public rangeDirection; // Direction of Range from meridian
+    string public county; // identifies the county where the land is located
+    uint public number; // Additional number added to the Tract ID
     address public owner; // The address of the current owner of the Tract ID
 
     // Event to track changes to the owner of the Tract ID
     event OwnershipChanged(address indexed previousOwner, address indexed newOwner);
 
     // Constructor function to set the initial values of the Tract ID
-    constructor(string memory _lac, string memory _sfx, string memory _plant, uint _section, uint _township, uint _range, address _owner) {
+    constructor(string memory _lac, string memory _sfx, string memory _plant, uint _section, uint _township, string memory _townshipDirection, uint _range, string memory _rangeDirection, string memory _county, uint _number, address _owner) {
         lac = _lac;
         sfx = _sfx;
         plant = _plant;
         section = _section;
         township = _township;
+        townshipDirection = _townshipDirection;
         range = _range;
+        rangeDirection = _rangeDirection;
+        county = _county;
+        number = _number;
         owner = _owner;
     }
 
